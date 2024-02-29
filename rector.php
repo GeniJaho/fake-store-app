3b28e3ec5a70ca95253ad90acbb79fa1cf645a3a
geni_jaho <jahogeni@gmail.com>
2024-02-29 18:33:08 +0100

    Setup Rector, Pint, and PHPStan

---
+++ rector.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\CodingStyle\Rector\ArrowFunction\StaticArrowFunctionRector;
+use Rector\CodingStyle\Rector\Closure\StaticClosureRector;
+use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
+use Rector\CodingStyle\Rector\PostInc\PostIncDecToPreIncDecRector;
+use Rector\Config\RectorConfig;
+use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
+use Rector\PHPUnit\Set\PHPUnitSetList;
+use Rector\Set\ValueObject\SetList;
+use RectorLaravel\Rector\ClassMethod\AddGenericReturnTypeToRelationsRector;
+use RectorLaravel\Set\LaravelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__.'/app',
+        __DIR__.'/config',
+        __DIR__.'/database',
+        __DIR__.'/tests',
+    ]);
+
+    $rectorConfig->importNames();
+
+    $rectorConfig->sets([
+        SetList::PHP_82,
+        SetList::CODE_QUALITY,
+        SetList::CODING_STYLE,
+        SetList::DEAD_CODE,
+        LaravelSetList::LARAVEL_100,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
+        LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
+        PHPUnitSetList::PHPUNIT_100,
+        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
+    ]);
+
+    $rectorConfig->rules([
+        AddGenericReturnTypeToRelationsRector::class,
+    ]);
+
+    $rectorConfig->skip([
+        AddLiteralSeparatorToNumberRector::class,
+        PostIncDecToPreIncDecRector::class,
+        StaticArrowFunctionRector::class,
+        StaticClosureRector::class,
+        EncapsedStringsToSprintfRector::class,
+        __DIR__.'/app/Http/Middleware/RedirectIfAuthenticated.php',
+    ]);
+
+    // Ensure file system caching is used instead of in-memory.
+    $rectorConfig->cacheClass(FileCacheStorage::class);
+
+    // Specify a path that works locally as well as on CI job runners.
+    $rectorConfig->cacheDirectory('./storage/rector/cache');
+};
