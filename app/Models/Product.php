ccf9300a0979db3e11544b57c2be5c5fb7c0f1c4
geni_jaho <jahogeni@gmail.com>
2024-02-29 18:51:12 +0100

    Clean up and fix PHPStan errors

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property float $price
+ */
 class Product extends Model
 {
     use HasFactory;
